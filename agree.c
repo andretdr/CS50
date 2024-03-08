215749a67ce56d61a6ddc90ae1396cfe10942665
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-08 06:14:34 +0000

    Fri, Mar 8, 2024, 6:14 AM +00:00

---
+++ agree.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char c = get_char ("Do you agree ?\n");
+    if (c == 'y')
+    {
+        printf ("YES");
+    }
+    else
+    {
+        if (c == 'n')
+        {
+           printf ("NO");
+        }
+    }
+
+}
