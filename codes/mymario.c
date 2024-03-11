f0d79c9473b55d875a81e5515b0b6f556ab6b4b7
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-11 06:55:35 +0000

    Mon, Mar 11, 2024, 2:55 PM +08:00

---
+++ codes/mymario.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int bricks;
+    do
+        {
+            bricks = get_int ("give me a number higher then 0 please! ");
+        }
+    while (bricks < 1)
+
+    
+
+}
