f9db37151052012bb2d58c6e3ffd23bfcb9670ee
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-11 08:45:28 +0000

    Mon, Mar 11, 2024, 4:45 PM +08:00

---
+++ mario/more/mymario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int totalBricks;
+    do
+        {
+            totalBricks = get_int ("give me a number higher then 0 please! ");
+        }
+    while (totalBricks < 1);
+
+    for (int i = 0; i < totalBricks; i++) // columns
+    {
+
+
+        for (int j = 0; j < 2*totalBricks+2; j++) // rows
+        {
+            int blanks = totalBricks - i-1; // number of blanks on this row
+            int bricks = totalBricks - blanks;
+            if ((j < blanks) || ((j - totalBricks - 1) > bricks) || (j == totalBricks) || (j == totalBricks + 1))
+                printf(" ");
+            else
+                printf("#");
+
+        }
+        printf ("\n");
+    }
+
+}
