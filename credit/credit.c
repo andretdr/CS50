2a6de385ea35f26b874accfe21735e2a42de45dd
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-11 09:14:06 +0000

    Mon, Mar 11, 2024, 5:14 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,26 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int typeCheck (long a); // 0=null, 1=Amex, 2=Master, 3=Visa
+//int typeCheck (long a); // 0=null, 1=Amex, 2=Master, 3=Visa
 
 int main(void)
 {
+    long cc;
     do
     {
-        long cc = get_long ("Enter CC number please: ");
+        cc = get_long ("Enter CC number please: ");
     }
     while (cc < 1);
 
-printf("%i\n",cc/10);
+printf("%li\n",cc/10);
 
 }
 
 
 
-
+/*
 int typeCheck (long a)
 {
 
 
 
 }
+*/
