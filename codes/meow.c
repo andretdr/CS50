f001f20cfdf1ac3f15ffb943f1c43da7b02f1d21
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-11 05:16:24 +0000

    Mon, Mar 11, 2024, 1:16 PM +08:00

--- codes/meow.c
+++ codes/meow.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void meow(void) // does nt return a value, and doesnt take in a parameter void, void
+    {
+        printf("meow \n");
+    }
+
+
+
+
 int main(void)
 {
     int counter = 0;
@@ -24,10 +32,6 @@ int main(void)
 
 // declaring functions
 
-void meow(void) // does nt return a value, and doesnt take in a parameter void, void
-    {
-        printf("meow \n");
-    }
 
 
 
