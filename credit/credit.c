119e0621f292e099f31252bd9f6611710ea68ccd
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-11 09:25:10 +0000

    Mon, Mar 11, 2024, 5:25 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,7 +12,7 @@ int main(void)
     }
     while (cc < 1);
 
-printf("%li\n",cc/10);
+    typeCheck (cc);
 
 
 
@@ -23,8 +23,12 @@ printf("%li\n",cc/10);
 
 int typeCheck (long a) // 0=null, 1=Amex, 2=Master, 3=Visa
 {
-    if (a/
-
-
+    long amexCheck = a/10000000000000;
+    // amex check
+    printf ("%li\n", amexCheck);
+    if ((amexCheck) == 34 || (amexCheck) == 37)
+        return 1;
+    else
+        return 0;
 }
 
