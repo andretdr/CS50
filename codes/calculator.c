b234398db32d4ab8684915984b2e57fafa2fa9e8
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-11 05:37:46 +0000

    Mon, Mar 11, 2024, 1:37 PM +08:00

--- codes/calculator.c
+++ codes/calculator.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int add(int x, int y);
+int subtract (int x, int y);
+
 int main(void)
 {
     int x = get_int ("x: ");
     int y = get_int ("y: ");
 
-    printf("x+y = %i\n", (x+y));
+    printf("x+y = %i\n", add(x,y));
+    printf("x-y = %i\n", subtract(x,y));
 }
 
 int add(int x, int y)
 {
-    
-
+    return (x+y);
 }
+
+int subtract (int x, int y)
+{
+    return (x-y);
+}
+
+
