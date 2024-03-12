11e50a6ef1b6d20c74676511339cb65cf4c971b3
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-12 07:48:58 +0000

    Tue, Mar 12, 2024, 3:48 PM +08:00

--- codes/uppercase.c
+++ codes/uppercase.c
@@ -6,15 +6,22 @@ int main(void)
 {
     string name = get_string ("Input  :");
 
+
+    printf("Output :");
+
     for (int i = 0, l = strlen(name); i < l; i++)
     {
-        
+        if (name[i]>='a' && name[i]<='z')
+        {
+            printf("%c",name[i] - 32);
+        }
+        else
+            printf("%c",name[i]);
 
 
 
     }
-
-
+    printf("\n");
 
 
 
