f8de03ee9688d76145f4735aa122666a116f45ea
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-12 14:01:41 +0000

    Tue, Mar 12, 2024, 10:01 PM +08:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main (void)
+{
+    int scoreboard[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10}
+
+    string inputa = get_string("Player 1: ");
+    string inputb = get_string("Player 2: ");
+
+
+
+}
