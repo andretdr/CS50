5ff6803720a8fccb83f3e7cb7692439c41893a62
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-12 02:42:53 +0000

    Tue, Mar 12, 2024, 10:42 AM +08:00

--- codes/scores.c
+++ codes/scores.c
@@ -1,21 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
+const int N = 3;
+
 int main(void)
 {
-/*    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
-*/
-    int score[3];
+    int score[N];
     int total = 0;
 
-    for (int i=0;i<3;i++)
+    for (int i=0;i<N;i++)
     {
         score[i]=get_int("gimme score :");
         total = total + score[i];
     }
 
-    printf("Average score %.2f\n",(total)/3.0);
+    printf("Average score %.2f\n",(total)/(float)N);
 
 }
