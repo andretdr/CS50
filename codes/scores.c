33f844da8762ec2122270eb0ea524535fb035857
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-12 02:34:41 +0000

    Tue, Mar 12, 2024, 10:34 AM +08:00

--- codes/scores.c
+++ codes/scores.c
@@ -3,10 +3,14 @@
 
 int main(void)
 {
-    int score1 = 72;
+/*    int score1 = 72;
     int score2 = 73;
     int score3 = 33;
+*/
 
+
+    int score[3];
+    
     printf("Average score %.2f\n",(72+73+33)/3.0);
 
 }
