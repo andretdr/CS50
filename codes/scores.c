57727dd170f2d9ea70650819cacd9ba6d5d829ad
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-12 02:31:40 +0000

    Tue, Mar 12, 2024, 10:31 AM +08:00

---
+++ codes/scores.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int score1 = 72;
+    int score2 = 73;
+    int score3 = 33;
+
+    printf("Average score %.2f\n",(72+73+33)/3.0);
+
+}
