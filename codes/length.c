af14f909ceb33c747ba48b7b1c40ab9ed8ee1665
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-12 07:12:51 +0000

    Tue, Mar 12, 2024, 3:12 PM +08:00

--- codes/length.c
+++ codes/length.c
@@ -6,9 +6,9 @@ int main(void)
     string name = get_string("Gimme name ");
 
     int n = 0;
-    while (name[n] != null)
+    while (name[n] != '\0')
         n++;
 
-    printf("Length is %i",n+1);
+    printf("Length is %i\n",n);
 
 }
