d25a2c79da7dc1f8cb76383b52d2bf29af5317cd
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-12 02:38:22 +0000

    Tue, Mar 12, 2024, 10:38 AM +08:00

--- codes/scores.c
+++ codes/scores.c
@@ -7,10 +7,13 @@ int main(void)
     int score2 = 73;
     int score3 = 33;
 */
-
-
     int score[3];
-    
+
+    for (int i=0;i<3;i++)
+    {
+        score[i]=get_int("gimme score :");
+    }
+
     printf("Average score %.2f\n",(72+73+33)/3.0);
 
 }
