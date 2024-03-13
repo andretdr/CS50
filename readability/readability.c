75d385ea7587904f6814ea896f92cdd73c5cdaa8
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-13 04:42:31 +0000

    Wed, Mar 13, 2024, 12:42 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,8 +7,22 @@ int main(void)
     int tletters = 0;
     int twords = 0;
     int tsentences = 0;
+    bool 
 
     string text = get_string ("Text: ");
+
+    // main loop
+    for (int i = 0, n = strlen (text); i < n; i++)
+    {
+        char c = text[i];
+        if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))  // if its a letter
+        {
+            tletter++;
+        }
+
+
+    }
+
 
     printf ("%s\n", text);
 
