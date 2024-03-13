86622011a35e50524988a15ba703af41511490aa
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-13 03:47:21 +0000

    Wed, Mar 13, 2024, 11:47 AM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 #include <string.h>
 
-string codify(string s);
+string lowercase(string s);
 int addtotal(string s);
 
 int main (void)
@@ -12,18 +12,18 @@ int main (void)
 
     int atotal = addtotal(codify(inputa));
 //    int btotal = addtotal(codify(inputb));
-    string test = codify(inputa);
+    string test = lowercase(inputa);
 //    printf("%i\n",atotal);
 //    printf ("%i %i %i %i\n",test[0],test[1],test[2],test[3]);
 }
 
-string codify(string s) //lowercases and converts letters to correspond to a=1, b=2 etc
+string lowercase(string s) //lowercases and converts letters to correspond to a=1, b=2 etc
 {
     for (int i = 0, n = strlen(s); i < n; i++)
     {
         if ((s[i] >= 'A') && (s[i] <= 'Z'))
         {
-            s[i] = s[i] + 32 - 97;
+            s[i] = s[i] + 32; // - 97;
         }
         else
             if ((s[i] >= 'a') && (s[i] <= 'z'))
@@ -40,7 +40,7 @@ int addtotal(string s) // add all the score according to score board
 {
     int scoreboard[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
     int total = 0;
-    
+
     for (int i = 0, n = strlen(s); i < n; i++) //for length of string
     {
         total = total + scoreboard[(int)s[i]]; // cast the char into an int
