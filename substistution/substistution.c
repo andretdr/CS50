de3b050e835118044eee62d5e175cffd4bff83a4
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-13 06:45:53 +0000

    Wed, Mar 13, 2024, 2:45 PM +08:00

--- substistution/substistution.c
+++ substistution/substistution.c
@@ -7,17 +7,25 @@ bool check(string s);
 
 int main(int argc, string argv[])
 {
+    int exitval = 0;
+
     if (argc == 2) // if it just accepted 1 additional string input
     {
         check(argv[1]);
     }
+    else
+    {
+        printf("Usage: ./substitution key");
+        exitval = 1;
+    }
 
-
+    return exitval;
 }
 
 bool check(string s)
 {
-    if
+    int n = strlen(s);
+    if 
 
     return true;
 }
