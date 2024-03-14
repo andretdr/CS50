88cf5c0069a01e600467ad1de7ee6c1b48843968
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-14 14:08:11 +0000

    Thu, Mar 14, 2024, 10:08 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -134,7 +134,21 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    // look through preferences and add into pairs
+
+    for (int i = 0; (i + 1) < candidate_count; i++)
+    {
+        for (int j = (i + 1); j < candidate_count; j++)
+        {
+            if (preferences[i][j] > 0) // if this pair is not tied
+            {
+
+            }
+        }
+    }
+
+
+
     return;
 }
 
