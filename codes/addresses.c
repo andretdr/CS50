a0bf884dd0d64ca86f7812c9e4b9dfa657c18329
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-15 14:17:31 +0000

    Fri, Mar 15, 2024, 10:17 PM +08:00

--- codes/addresses.c
+++ codes/addresses.c
@@ -13,10 +13,14 @@ int main(void)
     printf("%p\n",p); // p stores the address of n
     printf("%i\n",*p); // *p stores n (the value of n) goto the address of n
 */
-
+/*
     string s = "Hi!";
     printf("%p\n", s); // prints s which is a pointer to an address. so prints the address s points to
     printf("%p\n", &s[0]); // prints the address of the 1st char of s
 
     //char *s == string s you can replace
+    */
+
+   char *s = "HI!";
+   printf("%c", s[])
 }
