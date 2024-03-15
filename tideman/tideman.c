621c8902e6563913bca9e8de16b28173f2136931
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-15 06:31:05 +0000

    Fri, Mar 15, 2024, 2:31 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -226,11 +226,12 @@ void lock_pairs(void)
         int nextindex = pairs[i].loser;
 
         cycle = checkcycle(startingindex, nextindex);
-     
+
         if (!cycle) // entry of pairs[i] does not create cycle
         {
             // add
             locked[pairs[i].winner][pairs[i].loser] = true;
+            printf("locked[%i][%i] = true\n", pairs[i].winner, pairs[i].loser);
         }
     }
 
