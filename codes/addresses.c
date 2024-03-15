44db2a2aaeaff688ce9021250c5c7c5c7f49b6a1
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-15 13:48:05 +0000

    Fri, Mar 15, 2024, 9:48 PM +08:00

--- codes/addresses.c
+++ codes/addresses.c
@@ -4,6 +4,11 @@ int main(void)
 {
     int n = 50;
     int *p = &n; // p is pointer var that stores the address of a int. &n is the address of n
+    // when declaring a pointer, syntax is *p
+
+    // when using the pointer var, just use p to return address
+    // when want to GOTO the var address, use *p
+
 
     printf("%p\n",p); // p stores the address of n
     printf("%i\n",*p); // *p stores n (the value of n) goto the address of n
