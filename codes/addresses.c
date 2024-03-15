c94d14ea16f2bdcc8808e757e960d2a9a9ef90a7
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-15 13:43:36 +0000

    Fri, Mar 15, 2024, 9:43 PM +08:00

--- codes/addresses.c
+++ codes/addresses.c
@@ -5,7 +5,8 @@ int main(void)
     int n = 50;
     int *p = &n; // p is pointer var that stores the address of a int. &n is the address of n
 
-    printf("%p\n",p); // print address of n
+    printf("%p\n",p); // p stores the address of n
+    printf("%p\n",p); // *p stores n (the value of n)
     )
 
 }
