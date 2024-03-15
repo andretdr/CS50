7865d5e1873dc5be7356e89d1588b9a18e194af8
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-15 04:43:09 +0000

    Fri, Mar 15, 2024, 12:43 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -177,9 +177,9 @@ void sort_pairs(void)
     int n = pair_count;
 
     if (pair_count > 1) // only if more then 1 pair. or else its already 'sorted'
-        while ((!sorted) && (n ))
+        while ((!sorted) && (n > 2))
         {
-            for (int j = 0; j + 1 < n)
+            for (int j = 0; j + 1 < n; j++)
             {
                 // assume sorted
                 sorted = true;
