23504c2fe450ab8fb19f17d8c75309a97f1092f6
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-18 08:37:07 +0000

    Mon, Mar 18, 2024, 4:37 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -38,12 +38,19 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]); // converts the string into a float number. the factor to multiply by
 
     // Copy header from input file to output file//
+
+/*
     for (int i = 0, n = HEADER_SIZE; i < n; i++) // for each byte up to 44
     {
         fread(&b, sizeof(b), 1, src);
         fwrite(&b, sizeof(b), 1, dst);
     }
+*/
 
+    while (fread(&b, sizeof(b), 1, src) != 0) //fread each byte for you, reading it byte by byte to address of b, 1 byte at a time. while still have bytes to read
+    {
+        fwrite(&b, sizeof(b), 1, dst);
+    }
 
 
     // TODO: Read samples from input file and write updated data to output file
