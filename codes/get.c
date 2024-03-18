f2ad586361b3c77cbdc578af34a35d6f001b05eb
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-18 07:47:04 +0000

    Mon, Mar 18, 2024, 3:47 PM +08:00

--- codes/get.c
+++ codes/get.c
@@ -4,7 +4,10 @@
 
 int main(void)
 {
-    char *s; // pointers need to be initialised by malloc. the address needs to be assigned 
+    char *s; // pointers need to be initialised by malloc. the address needs to be assigned
+    // or else you can
+    // char s[4];
+
     printf("s: ");
     scanf("%s", s); // s is already an address, so still passing by reference
     printf("s: %s\n", s);
