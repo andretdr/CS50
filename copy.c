5d19622faa61b7e1a0708e1a9ef41d8f3a53b955
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-18 06:32:59 +0000

    Mon, Mar 18, 2024, 2:32 PM +08:00

--- copy.c
+++ copy.c
@@ -2,16 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h> // toupper tolower
+#include <stdlib.h>
 
 
 int main (void)
 {
     string s =get_string("s: ");
 
-    string t;
-    *t = *s; // now t has the same address
-    t[0] = toupper(t[0]);
+    string t = malloc(strlen(s) + 1); // +1 for the null char
 
+    *t = *s; // now t has the same address
+
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
 
     printf ("%s\n", s);
     printf ("%s\n", t);
