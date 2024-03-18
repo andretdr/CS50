8367a15e46ff9af2a74d188201d7ca6aba0ebb29
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-18 13:23:12 +0000

    Mon, Mar 18, 2024, 9:23 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -38,7 +38,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             RGBTRIPLE temptrip = image[i][j];
             BYTE btemp;
 
-            // while j < swap image[i][j] w image[i][width-j-1] until
+            // while j < width-j-1, swap image[i][j] w image[i][width-j-1] until
             float temp = (temptrip.rgbtBlue + temptrip.rgbtGreen + temptrip.rgbtBlue)/3; // need to round this please
 
             // rounding
