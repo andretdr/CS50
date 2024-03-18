fbc0b53d55de97e5df5e9b3b131293fd045689cb
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-18 07:30:24 +0000

    Mon, Mar 18, 2024, 3:30 PM +08:00

--- codes/swap.c
+++ codes/swap.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.h>
 
-void swap ();
+void swap (int *a, int *b);
 
 int main(void)
 {
     int x = get_int("gimme X: ");
     int y = get_int("gimme Y: ");
-    swap ();
+    swap (&x, &y);
 
     printf("X is now %i\n",x);
     printf("Y is now %i\n",y);
 
 }
 
-void swap ()
+void swap (int *a, int *b) // address of a, address of b
 {
-
-
+    int temp = *a; //*a means goto address of a
+    *a = *b;
+    *b = temp;
 }
