9c9ce21d0374a60b82aeb775f41ba8daab0cee67
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-18 13:29:10 +0000

    Mon, Mar 18, 2024, 9:29 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -33,28 +33,18 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0, n = height; i < n; i++)
-        for (int j = 0, k = width; j < k; j++)
-        {
-            RGBTRIPLE temptrip = image[i][j];
-            BYTE btemp;
-
+    {
+        BYTE btemp;
+        int j = 0;
+        int k = width;
             // while j < width-j-1, swap image[i][j] w image[i][width-j-1] until
-            float temp = (temptrip.rgbtBlue + temptrip.rgbtGreen + temptrip.rgbtBlue)/3; // need to round this please
-
-            // rounding
-            if ((temp - (int)temp) >= 0.5)
-            {
-                btemp = (int)temp + 1;
-            }
-            else
-            {
-                btemp = (int)temp;
-            }
-
-            image[i][j].rgbtBlue = btemp;
-            image[i][j].rgbtGreen = btemp;
-            image[i][j].rgbtRed = btemp;
-
+        while (j < (width - j - 1))
+        {
+            //swap
+            RGBTRIPLE temptrip = image[i][j];
+            image[i][j] = image[i][width-j-1];
+            image[i][width-j-1] = image[i][j];
+            j++;
         }
     return;
 }
