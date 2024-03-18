aaa52efa36e47113f6e9f0bd7d5207016d35dd0f
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-18 09:48:34 +0000

    Mon, Mar 18, 2024, 5:48 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -6,11 +6,11 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0, n = height; i < n; i++)
         for (int j = 0, k = width; j < k; j++)
         {
-            RGBTRIPLE temptrip = image[i][j];
-            BYTE temp = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3; // need to round this please
-            image[i][j].rgbtBlue = temp;
-            image[i][j].rgbtGreen = temp;
-            image[i][j].rgbtRed = temp;
+            RGBTRIPLE *temptrip = image[i][j];
+            BYTE temp = (&temptrip.rgbtBlue + &temptrip.rgbtGreen + &temptrip.rgbtBlue)/3; // need to round this please
+            temptrip.rgbtBlue = temp;
+            temptrip.rgbtGreen = temp;
+            temptrip.rgbtRed = temp;
         }
 
 
