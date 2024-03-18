cfae7344958c9c4d0bb5dda020fc1eaa1f8a679c
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-18 06:17:57 +0000

    Mon, Mar 18, 2024, 2:17 PM +08:00

--- compare.c
+++ compare.c
@@ -3,6 +3,15 @@
 
 int main(void)
 {
+    string s = get_string("s: ");
+    string t = get_string("t: ");
 
-    
+    if (s == t)
+    {
+        printf("Same\n");
+    }
+    else
+    {
+        printf("Same\n");
+    }
 }
