5415cff5b3b024284457adca85bce23874a1e2f4
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-19 14:11:37 +0000

    Tue, Mar 19, 2024, 10:11 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,13 +3,27 @@
 
 int main(int argc, char *argv[])
 {
+    typedef
+
+
     if (argc != 2)
     {
-        printf("Usage: ./recover image");
+        printf("Usage: ./recover image\n");
         return 1;
     }
-    else
+
+    // Open file
+    FILE *image = fopen(argv[1], "r");
+    if (input == NULL)
     {
-        
+        printf("Could not open file.\n");
+        return 1;
     }
+
+
+
+
+
+
+    return 0;
 }
