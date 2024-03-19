eece040bc9cf8ccf3d197b14d70bbe11308c8453
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-19 14:06:58 +0000

    Tue, Mar 19, 2024, 10:06 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image");
+        return 1;
+    }
+    else
+    {
+        
+    }
 }
