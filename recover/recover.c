b76c27cfd9fc896607606fc5fbc74275cc17827d
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-20 15:09:46 +0000

    Wed, Mar 20, 2024, 11:09 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     int blockcount = 0;
     int n = 0;
     const int namelength = 8;
-    char filename[namelength] = {"000.jpg"};
+    char filename[namelength] = {"000.jpg"}; // memleak error, syscall param openat(filename) ... I dint initialize this string
     FILE *photo = fopen(filename, "w");
     if (photo == NULL)
     {
