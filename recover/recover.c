11789cd2554841bcb38ad6084b9ba36de27b7d74
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-20 08:15:32 +0000

    Wed, Mar 20, 2024, 4:15 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -47,11 +47,9 @@ int main(int argc, char *argv[])
             if ((buffer[0] == 255) && (buffer[1] == 216) && (buffer[2] == 255) && (buffer[3] >= 224) && (buffer[3] <= 239))
             {
                 n++;
-                printf("%i\n",n);
-
+                opennew = true;
             }
             blockcount = 0;
-            
         }
     }
 
