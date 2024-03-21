4b37553a7d0dcac0b06a8692a7ae97107782dc9e
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-21 09:00:30 +0000

    Thu, Mar 21, 2024, 5:00 PM +08:00

--- codes/try.c
+++ codes/try.c
@@ -1,2 +1,7 @@
 // a tree of arrays
 
+typedef struct node // it doesnt store the key, or the name. the arrays point to the 'letter' or index
+{
+    struct node *children[26];
+    char *number;
+} node;
