0ca84380cb753df9e5be3f8f6e4fb9a1fa26490f
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-21 14:47:05 +0000

    Thu, Mar 21, 2024, 10:47 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -13,7 +13,7 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 456976; //26 ^ 4
 
 // Hash table
 node *table[N];
