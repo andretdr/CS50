da8801bd2cf495563cfcde151b28321040e109b7
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-21 14:24:20 +0000

    Thu, Mar 21, 2024, 10:24 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -21,7 +21,7 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    // TODO check the word against the hash table dictionary
     return false;
 }
 
@@ -35,7 +35,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary) // dictionary is the file name
 {
-    // create the hashtable and load the dictionary into it
+    // TODO create the hashtable and load the dictionary into it
     return false;
 }
 
