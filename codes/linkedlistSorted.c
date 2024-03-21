7b989360c1983eed0743d5b76ac9371668238426
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-21 08:07:01 +0000

    Thu, Mar 21, 2024, 4:07 PM +08:00

--- codes/linkedlistSorted.c
+++ codes/linkedlistSorted.c
@@ -5,3 +5,26 @@ if (list == NULL)
 {
     list = n;
 }
+else
+    if (n->number < list->number) // if number is at start
+    {
+        n->next = list;
+        list = n;
+    }
+    else // iterate
+    {
+        for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+        {
+            if (ptr->next == NULL) // If at the end
+            {   // append
+                ptr->next = n;
+                break;
+            }
+            if (n->number < ptr->next->number)
+            {
+                n->next = ptr->next;
+                ptr->next = n;
+                break;
+            }
+        }
+    }
