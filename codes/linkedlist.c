800003be20a2fb66053dc681534d425924607c42
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-21 07:42:03 +0000

    Thu, Mar 21, 2024, 3:42 PM +08:00

--- codes/linkedlist.c
+++ codes/linkedlist.c
@@ -11,9 +11,25 @@ int main(int argc, char *argv[])
 {
     node *list = NULL; // the first pointer
 
+    for (int i = 1; i < argc; i++)
+    {
+        int value = atoi(argv[i]); //convert string to int
+
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            // free memory
+            return 1;
+        }
+        n->number = value;
+
+    }
+
     //add a new node
-    node *n = malloc(sizeof(node));
+
     n->number = 1; // (*n).number = 1; same
     n->next = NULL;
+
+    free(node);
 
 }
