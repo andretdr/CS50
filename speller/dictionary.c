f1a7f771ef5bd6105111c554d64381c4cf3e0999
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-22 09:16:26 +0000

    Fri, Mar 22, 2024, 5:16 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -93,9 +93,8 @@ bool load(const char *dictionary) // dictionary is the file name. my dictionary 
                 for (int i = 0; i < index; i++)
                 {
                     n->word[i] = word[i];
-                    printf("%c",word[i]);
                 }
-
+                
                 bool exit = add(table[hash(word)], n);
 
                 // test
