abdb405be5f3429e99d020bae5e71d9811a1d273
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-22 13:42:35 +0000

    Fri, Mar 22, 2024, 9:42 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -153,8 +153,8 @@ bool add(node **current, node **toadd)
         return true;
     }
     else
-sd
-        if (strcmp(*(toadd->word),*(current->word)) <= 0) // if toadd node is smaller then current node, then add it here
+
+        if (strcmp(**(toadd->word),*(current->word)) <= 0) // if toadd node is smaller then current node, then add it here
         {
             *(toadd->next) = *current;
             *current = *toadd;
