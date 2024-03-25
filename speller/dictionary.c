9ebb2f1c96d801a2e964570f62eec770c5551a72
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-25 09:04:35 +0000

    Mon, Mar 25, 2024, 5:04 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -121,7 +121,8 @@ bool load(const char *dictionary) // dictionary is the file name. my dictionary 
                 // initialise node
                 node *n = malloc(sizeof(node));
                 n->next = NULL;
-                //n->word = ;
+
+                //n->word = NULL;
                 for (int i = 0; i < index; i++)
                 {
                     n->word[i] = word[i];
