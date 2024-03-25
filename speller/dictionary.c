9389559132fe438c7a1074825afb9ceb02cbee25
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-25 10:03:10 +0000

    Mon, Mar 25, 2024, 6:03 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -296,8 +296,9 @@ void unloadr(node *current) // recursively unloads
     else
     {
         unloadr(current->next);
-        
+
         free(current->next);
+        
         printf("unloading %s\n",current->word);
     }
 }
