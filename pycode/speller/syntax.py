e22d39dcea8c8f9b558e7dc82095cc6fe078057f
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-26 14:29:55 +0000

    Tue, Mar 26, 2024, 10:29 PM +08:00

--- pycode/speller/syntax.py
+++ pycode/speller/syntax.py
@@ -4,6 +4,6 @@ import cs50 # whole library
 from cs50 import get_string # just the one function
 
 # functions
-answer = get_string("What's your name? ")
-print("hello, " + answer)
+answer = get_string("What's your name? ") # dont have to declare type of variable
+print("hello, " + answer) # joining strings together
 
