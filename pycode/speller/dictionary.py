78a7ad0f68db8e5a62b505aa16180960ba5c778b
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-26 14:11:45 +0000

    Tue, Mar 26, 2024, 10:11 PM +08:00

--- pycode/speller/dictionary.py
+++ pycode/speller/dictionary.py
@@ -1,6 +1,18 @@
 words = set() # variable words, of a set without duplicates
 
 def check(word): # define a function
+    return word.lower() in words
+
+def load(dictionary):
+    with open(dictionary) as file:
+        words.update(file.read().splitlines())
+    return True
+
+def size():
+    return len(words)
+
+def unload():
+    return True
 
 
 
