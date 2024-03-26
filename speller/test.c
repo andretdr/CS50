55ddc98d7ba49d70badfce15a1f0b85616de829b
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-26 08:05:41 +0000

    Tue, Mar 26, 2024, 4:05 PM +08:00

---
+++ speller/test.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[2];
+    struct node *next;
+} node;
+
+int main void()
+{
+    node *table[5] = {NULL};
+    node *n = malloc(sizeof(node));
+    n->word[0] = 'h';
+    n->word[1] = '\0';
+    table[0]->next = n;
+    printf("%c\n",*(*(table[0]).next).word);
+
+    free(n);
+}
