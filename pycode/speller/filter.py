a4e7a786a7bb4aed89b8a34fb9331f1187885b20
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-26 14:18:43 +0000

    Tue, Mar 26, 2024, 10:18 PM +08:00

---
+++ pycode/speller/filter.py
@@ -0,0 +1,10 @@
+from PIL import Image, ImageFilter # python image library
+
+before = Image.open("bridge.bmp") # before is variable
+after = before.filter(ImageFilter.BoxBlur(10))
+after.save("out.bmp")
+
+
+before = Image.open("bridge.bmp")
+after = before.filter(ImageFilter.FIND_EDGES)
+after.save("out.bmp")
