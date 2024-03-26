cbd8e0f119e9b9b0236ff2714b30c9195061289a
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-26 08:09:47 +0000

    Tue, Mar 26, 2024, 4:09 PM +08:00

--- speller/test.c
+++ speller/test.c
@@ -17,12 +17,12 @@ int main (void)
 
 
 
-    node *table[5] = {NULL};
+    node *table[5];
     node *n = malloc(sizeof(node));
     n->word[0] = 'h';
     n->word[1] = '\0';
     table[0]->next = n;
-    printf("%c\n",(*(*table[0]).next).word);
+    printf("%s\n",(*(*table[0]).next).word);
 
     free(n);
 }
