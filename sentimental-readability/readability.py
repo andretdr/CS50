b911e1415754183a3e1d530524ebeaf2e01f10ee
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-27 14:58:25 +0000

    Wed, Mar 27, 2024, 10:58 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,10 +11,14 @@ def main():
 
     sentences = re.findall('[.!?]', text) # (r'\.', text)
     words = re.findall('[ .!?]', text)
-    letters = re.findall('[a-z, A-Z]', text)
-    for i in space:
-        print(f"{i}")
-    print(f"{len(space)}")
+    letters = re.findall('[a-zA-Z]', text)
+#    for i in space:
+#        print(f"{i}")
+    print(f"{len(sentences)}")
+    print(f"{len(words)}")
+    print(f"{len(letters)}")
+
+
 
 main()
 
