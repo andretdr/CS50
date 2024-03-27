70770451589990020e11e6a4fa97cd97081fa751
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-27 14:34:55 +0000

    Wed, Mar 27, 2024, 10:34 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -9,10 +9,10 @@ import re
 def main():
     text = input("Text: ")
 
-    space = re.findall("[.{1,} ]", text)
+    space = re.findall(".{1,}", text)
     for i in space:
         print(f"{i}")
-
+    print(f"{len(space)}")
 
 main()
 
