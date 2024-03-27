324278cf17e84297213dc437a13b73520fc4af9f
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-27 09:02:40 +0000

    Wed, Mar 27, 2024, 5:02 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,8 +1,14 @@
-def
+def get_int(prompt):
+    while True
+        try:
+            value = int(input(prompt))
+            
+        exception ValueError:
+            print("INVALID")
 
 
 def main():
-    
+
 
 
 
