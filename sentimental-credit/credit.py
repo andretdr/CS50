f51ae46bb8038c16fe4302389ee9264e426a4f5c
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-27 09:26:16 +0000

    Wed, Mar 27, 2024, 5:26 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -13,6 +13,7 @@ def get_cc(prompt):
 def main():
     number = get_cc("Number: ")
 
+    
 
     if (re.search("^34", number) or (re.search("^37", number)) and len(number) == 15):
         print(f"AMEX")
