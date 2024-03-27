f5e5a86d5cd21a8dd8d5d6ea32279f31175ddb27
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-27 09:37:23 +0000

    Wed, Mar 27, 2024, 5:37 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -13,12 +13,16 @@ def get_cc(prompt):
 def main():
     number = get_cc("Number: ")
 
+    
+
 
 
     if (re.search("^34", number) or (re.search("^37", number)) and len(number) == 15):
         print(f"AMEX")
     elif (re.search("^[5][1-5]", number) and len(number) == 16):
         print(f"MC")
+    elif (re.search("^4", number) and (len(number) == 13 or (len(number) == 16))):
+        print(f"VISA")
     else:
         print("INVALID")
 
