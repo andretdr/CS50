1a40e27b812d3715b0d1f58559b3a0f878fe8939
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-28 08:00:18 +0000

    Thu, Mar 28, 2024, 4:00 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,24 +1,24 @@
 import re
 
-#def
-
-
-
-
-
 def main():
+
     text = input("Text: ")
 
     sentences = re.findall('[.!?]', text) # (r'\.', text)
-    words = re.findall('.', text)
+    words = re.findall(r"\w+[. !?,\"]", text)
     letters = re.findall('[a-zA-Z]', text)
-#    for i in space:
-#        print(f"{i}")
-    print(f"{len(sentences)}")
-    print(f"{len(words)}")
-    print(f"{len(letters)}")
 
+    L = len(letters)/len(words) * 100
+    S = len(sentences)/len(words) * 100
 
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+
+    if index < 1:
+        print("Before Grade 1")
+    elif (index >= 16):
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
 
 main()
 
