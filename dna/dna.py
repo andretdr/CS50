32624a71948f3c06f265eca3a554cd32b09b9e7c
andretdr <162653875+andretdr@users.noreply.github.com>
2024-03-28 08:21:30 +0000

    Thu, Mar 28, 2024, 4:21 PM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -1,13 +1,23 @@
 import csv
 import sys
+# from sys import argv
+
+def cmdlineinput():
+if len(sys.argv) != 3:
+    print(f"Incorrect cmdline input")
+
+else:
+
 
 
 def main():
 
+    cmdlineinput()
+
     # TODO: Check for command-line usage
 
     # TODO: Read database file into a variable
-    
+
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
@@ -43,16 +53,15 @@ def longest_match(sequence, subsequence):
             # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-            
+
             # If there is no match in the substring
             else:
                 break
-        
+
         # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
     # After checking for runs at each character in seqeuence, return longest run found
     return longest_run
-
 
 main()
