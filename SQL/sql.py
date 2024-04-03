aa61f8e90ece9e0feb6237e891d193387d38f140
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-03 03:26:46 +0000

    Wed, Apr 3, 2024, 11:26 AM +08:00

--- SQL/sql.py
+++ SQL/sql.py
@@ -2,4 +2,10 @@
 
 
 
-sqlite3 favorites.db # in terminal
+sqlite3 favorites.db # in terminal to create and load yr csv file into a db file
+
+.mode csv
+.import favorites.csv favorites # table name load the whole CSV ito favorites db file
+.quit
+
+# ls will show you the new db file
