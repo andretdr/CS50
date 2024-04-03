448f439cf1900ee1bc356929bd8636130360bac3
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-03 03:34:00 +0000

    Wed, Apr 3, 2024, 11:34 AM +08:00

--- SQL/sql.py
+++ SQL/sql.py
@@ -1,6 +1,6 @@
 #
 
-CREATE TABLE table (column type, ...);
+#syntax CREATE TABLE table (column type, ...);
 
 sqlite3 favorites.db # in terminal to create and load yr csv file into a db file
 
@@ -13,4 +13,8 @@ sqlite3 favorites.db # in terminal to create and load yr csv file into a db file
 sqlite3 favorites.db # open the db file
 .schema # shows the schema of the table
 
-SELECT columns FROM table; # upper case the keyword cmds, lowercase the placeholders
+#syntax SELECT columns FROM table; # upper case the keyword cmds, lowercase the placeholders
+
+SELECT * FROM favorites;
+
+
