2ad9d294fa7716618a1ec68a9e773ae61fc2f828
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-04 09:39:10 +0000

    Thu, Apr 4, 2024, 5:39 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -43,7 +43,7 @@ SELECT name FROM people WHERE passport_number IN (SELECT passport_number FROM pa
 
 -- intersecting them all
 SELECT name FROM people WHERE license_plate IN
-    (SELECT license_plate FROM bakery_security_logs WHERE year = '2023' AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25)
+    (SELECT license_plate FROM bakery_security_logs WHERE year = '2023' AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25)
     INTERSECT
     SELECT people.name FROM people, bank_accounts WHERE people.id = bank_accounts.person_id AND bank_accounts.account_number IN
     (SELECT account_number FROM atm_transactions WHERE year = 2023 AND month = 7 and day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw')
