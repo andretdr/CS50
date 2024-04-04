028ec08bf0aa4b04dc31a8485381c529f031e1e7
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-04 09:03:48 +0000

    Thu, Apr 4, 2024, 5:03 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -40,3 +40,20 @@ SELECT * FROM airports WHERE id = 4;
 SELECT passport_number FROM passengers WHERE flight_id = 36;
 -- match with people
 SELECT name FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 36);
+
+-- intersecting them all
+SELECT name FROM people WHERE license_plate IN
+    (SELECT license_plate FROM bakery_security_logs WHERE year = '2023' AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25)
+    INTERSECT
+    SELECT people.name FROM people, bank_accounts WHERE people.id = bank_accounts.person_id AND bank_accounts.account_number IN
+    (SELECT account_number FROM atm_transactions WHERE year = 2023 AND month = 7 and day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw')
+    INTERSECT
+    SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60)
+    INTERSECT
+    SELECT name FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 36);
+
+    -- BRUCE
+
+
+
+
