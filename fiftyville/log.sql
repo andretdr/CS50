ea3cca62c537c5d365d4d5aef161ae2d8617815a
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-04 07:43:58 +0000

    Thu, Apr 4, 2024, 3:43 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,4 +4,7 @@
 SELECT * FROM crime_scene_reports WHERE year = '2023' AND month = 7 AND street = 'Humphrey Street';
 -- Theft happened at 1015 at bakery. 3 witnesses mentioned the bakery
 
+-- checking bakery logs on the 28th, manually checking aruond 1015
+SELECT * FROM bakery_security_logs WHERE year = '2023' AND month = 7 AND day = 28;
+-- afew cars SELECT * FROM bakery_security_logs WHERE year = '2023' AND month = 7 AND day = 28;
 
