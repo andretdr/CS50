1f7264fbdb0a011d0069785e0348bb9f75508537
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-04 07:52:58 +0000

    Thu, Apr 4, 2024, 3:52 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,11 +6,9 @@ SELECT * FROM crime_scene_reports WHERE year = '2023' AND month = 7 AND street =
 
 -- Check interview logs
 SELECT * FROM interviews WHERE year = 2023 AND month = 7 AND day = 28;
--- within 10 minutes of the theft, so 1015 - 1025
+-- within 10 minutes of the theft, so 1015 - 1025, he got into a car n left
 -- earlier that morning the theif was at ATM on leggett street. Eugene recognises him
 -- before he left, 1015 - 1025 he called someone, less then a minute to buy a flight ticket. flight on 29th, earliest flight
-
-
 
 -- checking bakery logs on the 28th, manually checking aruond 1015
 SELECT * FROM bakery_security_logs WHERE year = '2023' AND month = 7 AND day = 28;
