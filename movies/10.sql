dd779d91d7685e86d1549ccb8fc91aa526ce42f8
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-04 07:21:05 +0000

    Thu, Apr 4, 2024, 3:21 PM +08:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT(people.name) FROM movies, people, directors, rating WHERE movies.id = directors.movie_id AND people.id = directors.person_id AND movies.id = ratings.movie_id AND ratings.rating >= '9.0';
+SELECT DISTINCT(people.name) FROM movies, people, directors, ratings WHERE movies.id = directors.movie_id AND people.id = directors.person_id AND movies.id = ratings.movie_id AND ratings.rating >= '9.0';
