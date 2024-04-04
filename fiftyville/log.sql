3fda003dab8bf001a9637d0c9cad962017ab1518
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-04 07:40:43 +0000

    Thu, Apr 4, 2024, 3:40 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Understanding what happened
+SELECT * FROM crime_scene_reports WHERE year = '2023' AND month = 7 AND street = 'Humphrey Street';
+-- Theft happened at 1015 at bakery. 3 witnesses mentioned the bakery
+
+
