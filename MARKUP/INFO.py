6bb96c6478df57984bd702556e5112e98430163d
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-05 13:52:13 +0000

    Fri, Apr 5, 2024, 9:52 PM +08:00

--- MARKUP/INFO.py
+++ MARKUP/INFO.py
@@ -22,7 +22,7 @@ GET / HTTP/2
 Host: www.harvard.edu    # http header
 
 # server response
-HTTP/2 200    # acknowledgment of version being used, status, 200 means OK, 301 means moved permanently
+HTTP/2 200    # acknowledgment of version being used, status, 200 means OK, 301 means moved permanently, 404 file not found
 Content-Type: text/html    # what kind of content?
 
 # terminal
