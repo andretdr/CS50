4ad0cb21f3547fb7faec39fc44c382d07eaeb56a
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-11 06:49:37 +0000

    Thu, Apr 11, 2024, 2:49 PM +08:00

--- flask/hello/app.py
+++ flask/hello/app.py
@@ -4,4 +4,5 @@ app = Flask(__name__) # to initiate this for web app
 
 @app.route("/") # this app's function, when user visits / will
 def index(): # run the function index, which just returns 'hello world'
-    return "hello, world"
+#    return "hello, world"
+    return render_template("index.html")
