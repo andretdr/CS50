9dfe269c389c6b8dc2ca11c9d5d7b80da16b06dd
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-11 07:11:31 +0000

    Thu, Apr 11, 2024, 3:11 PM +08:00

--- flask/hello/app.py
+++ flask/hello/app.py
@@ -7,7 +7,10 @@ def index(): # run the function index, which just returns 'hello world'
 #    return "hello, world"  # returns just this line
 #    return render_template("index.html")  # returns the static html in my temple folder, index.html
 
-                        # request.args returns a dictionary of keyvalue pairs ?key=value&key=value from the URL
-    name = request.args["name"] # create a variable called name, give it the value whos key is name
+    if "name" in request.args:  # if there is a 'name' value pair request
+                                    # request.args returns a dictionary of keyvalue pairs ?key=value&key=value from the URL
+        name = request.args["name"] # create a variable called name, give it the value whos key is name
+    else:                       # else just call 'world'
+        name = "world"
                                                             # python supports named parameters, specify the name of the parameter
     return render_template("index.html", placeholder=name)  # giving the parameter placeholder the value of the name var i just defined
