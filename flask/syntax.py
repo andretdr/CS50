fbe03e50e56dcc5aeb6bd484ebe1aa33acbcbebc
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-11 08:16:20 +0000

    Thu, Apr 11, 2024, 4:16 PM +08:00

--- flask/syntax.py
+++ flask/syntax.py
@@ -2,7 +2,8 @@
 
 # https://www.example.com/route?key=value&key=value ?key=value turns your static webpage into dynamic app
 
-GET /search?q=cats HTTP/2     #search is the name of google's web application, q is key, cats is value
+                            # GET puts the key value pairs in the URL, in yr HTML code
+GET /search?q=cats HTTP/2   #search is the name of google's web application, q is key, cats is value
 Host: www.google.com
 
 # Framwework => flask, django micro framework
@@ -18,3 +19,6 @@ templates/ #actual html, css, js files
 
 # request.args
 # look up jinja
+
+
+POST
