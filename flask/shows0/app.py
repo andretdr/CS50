44403c49eb5d7c0d2eb2993ad850a8df6048627e
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-15 06:40:53 +0000

    Mon, Apr 15, 2024, 2:40 PM +08:00

---
+++ flask/shows0/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect, session, render_html
+from flask_session import Session
+import cs50
+
+app = (__name__)
+
+Sessions
+
+db = ('///shows.db')
+
+@app.route = ("/")
+def 
