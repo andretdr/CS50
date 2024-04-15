04f25f0e055a0742b834054d5a16b36cd683ce99
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-15 06:48:53 +0000

    Mon, Apr 15, 2024, 2:48 PM +08:00

--- flask/shows0/app.py
+++ flask/shows0/app.py
@@ -1,15 +1,13 @@
-from flask import Flask, redirect, session, render_html
-from flask_session import Session
+from flask import Flask, redirect, render_template, request
 from cs50 import SQL
 
 app = Flask(__name__)
-
-app.config("SESSION_PERMANENT") = False
-app.config("SESSION_TYPE") = "filesystem"
-Session(app)
 
 db = SQL("sqlite:///shows.db")
 
 @app.route = ("/")
 def index():
-    
+    return render_template("index.html")
+
+
+
