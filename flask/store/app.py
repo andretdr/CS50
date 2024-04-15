507b87ea6919795ed7e0e6c3aa44ce6543ff00b0
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-15 04:36:07 +0000

    Mon, Apr 15, 2024, 12:36 PM +08:00

--- flask/store/app.py
+++ flask/store/app.py
@@ -1,8 +1,17 @@
-import Flask from flask redirect, session
-import Session from Flask
+from cs50 import SQL
+from flask import Flask, redirect, session, render_template, request
+from flask_session import Session
 
-@app = (__name__)
+app = Flask(__name__)
+
+db = SQL("sqlite:///store.db")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("\")
+def index():
+    
 
 @app.route("\login")
