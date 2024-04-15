f5148f40e5933c707b993771cdce3073af0d072f
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-15 06:54:42 +0000

    Mon, Apr 15, 2024, 2:54 PM +08:00

--- flask/shows0/app.py
+++ flask/shows0/app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 db = SQL("sqlite:///shows.db")
 
-@app.route = ("/")
+@app.route("/")
 def index():
     return render_template("index.html")
 
-
-
+@app.route("/search")
+def search():
+    shows = db.execute("SELECT * FROM shows WHERE title = (?)", request.args.get("q"))
+    return render_template("search.html", shows=shows)
