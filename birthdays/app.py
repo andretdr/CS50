cd03b89934a9e26aad366321aca99400b76a064f
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-16 13:53:33 +0000

    Tue, Apr 16, 2024, 9:53 PM +08:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -13,11 +13,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
 
-def bool validatedate(strdate)
-{
-    return true
-}
-
+def validatedate(strdate):
+    return True
 
 @app.after_request
 def after_request(response):
@@ -35,8 +32,9 @@ def index():
         record = {}
         record['name'] = request.form.get('name')
         record['date'] = request.form.get('date')
-        print(record['name'])
-        print(record['date'])
+        if validatedate(record['date']):
+            print(record['name'])
+            print(record['date'])
 
         return redirect("/")
 
