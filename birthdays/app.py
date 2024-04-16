3c517423808caffc7edebc89744090ef8cd24736
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-16 04:15:50 +0000

    Tue, Apr 16, 2024, 12:15 PM +08:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -1,7 +1,7 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, json, redirect, render_template, request, session
 
 # Configure application
 app = Flask(__name__)
@@ -35,8 +35,6 @@ def index():
         # TODO: Display the entries in the database on index.html
 
         birthdays = db.execute("SELECT * FROM birthdays")
-
-        print(jsonify(birthdays))
 
         return render_template("index.html", birthdays=json.dumps(birthdays))
 
