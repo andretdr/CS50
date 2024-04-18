5943aa3a893d2e7b963bf13026ea6d5a22083140
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-18 04:05:06 +0000

    Thu, Apr 18, 2024, 12:05 PM +08:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -112,7 +112,7 @@ def update():
     record['date'] = request.args.get('date')
 
     if (validateid(record['id']) and validatename(record['name']) and validatedate(record['date'])):
-        db.execute("UPDATE birthdays SET name = ? date = ? WHERE id = ?;", record['name'], record['date'], record['id'])
+        db.execute("UPDATE FROM birthdays SET name = ? date = ? WHERE id = ?;", record['name'], record['date'], record['id'])
         status = ''
     else:
         status = 'Invalid Entry'
