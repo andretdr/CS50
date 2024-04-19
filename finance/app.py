a8a1d67fe7bfdbbe0863a6fdae7d62270d8b764b
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-19 09:11:54 +0000

    Fri, Apr 19, 2024, 5:11 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session, jso
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd, validatename, validatepassword, addrecord
+from helpers import apology, login_required, lookup, usd, validatename, validatepassword, validatesymbol, addrecord
 
 # Configure application
 app = Flask(__name__)
@@ -103,6 +103,7 @@ def quote():
 
         if validatesymbol(record['symbol']) == 1:
             return jsonify({"status":"Symbol must be alphanumeric"})
+        
         result = lookup(record['symbol'])
         print(result)
         return jsonify(result)
