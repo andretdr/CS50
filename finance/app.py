7bf9b0c26f23e4ff3e106fb6c84b1bdc41f319d7
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-19 07:36:45 +0000

    Fri, Apr 19, 2024, 3:36 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -57,6 +57,8 @@ def logincheck:
 
     record = request.get_json();
 
+
+    # Ensure username was submitted
     if not request.form.get("username"):
         return apology("must provide username", 403)
 
@@ -66,20 +68,20 @@ def logincheck:
 
     # Query database for username
     rows = db.execute(
-        "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        "SELECT * FROM users WHERE username = ?", record['username']
     )
 
     # Ensure username exists and password is correct
     if len(rows) != 1 or not check_password_hash(
-        rows[0]["hash"], request.form.get("password")
+        rows[0]["hash"], record['password']
     ):
         return apology("invalid username and/or password", 403)
 
     # Remember which user has logged in
     session["user_id"] = rows[0]["id"]
 
-    # Redirect user to home page
-    return redirect("/")
+        # Redirect user to home page
+        return redirect("/")
 
 
 
