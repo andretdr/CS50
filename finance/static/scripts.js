d0db120f161b66e9d12e3c79453cada88d06ec57
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-22 06:40:31 +0000

    Mon, Apr 22, 2024, 2:40 PM +08:00

--- finance/static/scripts.js
+++ finance/static/scripts.js
@@ -1,10 +1,11 @@
 
 async function updateMainBalance() { // assume loggedin
-    let response = await fetch('/checkbalance');
+    let response = await fetch('/checktotal');
     let responset = await response.text();
     let responseObj = JSON.parse(responset);
     console.log(responseObj['balance']);
 //    document.querySelector("accountbalance").innerHTML =
+    return responseObj;
 
 }
 
