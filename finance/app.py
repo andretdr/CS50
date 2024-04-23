8002701db9b0912779e0effef4825f7e568c2447
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-23 06:05:31 +0000

    Tue, Apr 23, 2024, 2:05 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -6,7 +6,10 @@ from flask import Flask, flash, redirect, render_template, request, session, jso
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd, validatename, validatepassword, validatesymbol, addrecord, getbalance, strictlydigits, returnsymbol, update_transaction, update_portfolio
+from helpers import apology, login_required, lookup, usd, validatename, validatepassword, validatesymbol, addrecord, getbalance
+from helpers import strictlydigits, returnsymbol, update_transaction, update_portfolio
+
+
 # Configure application
 app = Flask(__name__)
 
