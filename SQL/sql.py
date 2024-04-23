1fb8b63baabd7d9a3307d14f1e63d4d4984394e7
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-23 01:40:28 +0000

    Tue, Apr 23, 2024, 9:40 AM +08:00

--- SQL/sql.py
+++ SQL/sql.py
@@ -192,3 +192,37 @@ CREATE INDEX name_index ON people (name);
 SELECT name FROM songs WHERE name LIKE '%feat%';
 
 https://www.w3schools.com/sql/sql_wildcards.asp
+
+# EXAMPLE .schema movies
+CREATE TABLE directors (
+    movie_id INTEGER NOT NULL,
+    person_id INTEGER NOT NULL,
+    FOREIGN KEY(movie_id) REFERENCES movies(id),
+    FOREIGN KEY(person_id) REFERENCES people(id)
+);
+CREATE TABLE movies (
+    id INTEGER,
+    title TEXT NOT NULL,
+    year NUMERIC,
+    PRIMARY KEY(id)
+);
+CREATE TABLE people (
+    id INTEGER,
+    name TEXT NOT NULL,
+    birth NUMERIC,
+    PRIMARY KEY(id)
+);
+CREATE TABLE ratings (
+    movie_id INTEGER NOT NULL,
+    rating REAL NOT NULL,
+    votes INTEGER NOT NULL,
+    FOREIGN KEY(movie_id) REFERENCES movies(id)
+);
+CREATE TABLE stars (
+    movie_id INTEGER NOT NULL,
+    person_id INTEGER NOT NULL,
+    FOREIGN KEY(movie_id) REFERENCES movies(id),
+    FOREIGN KEY(person_id) REFERENCES people(id)
+);
+
+
