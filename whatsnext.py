3505c36b6f24437982df55b9b7f9f3dff4be4976
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-24 07:07:48 +0000

    Wed, Apr 24, 2024, 3:07 PM +08:00

--- whatsnext.py
+++ whatsnext.py
@@ -5,3 +5,12 @@ version control
 
 https://youtu.be/MJUJ4wbFm_A
 
+# Download VS Code
+
+https://code.visualstudio.com/
+https://cs50.readthedocs.io/cs50.dev/
+
+# Host web site
+
+https://pages.github.com/
+https://www.netlify.com/
