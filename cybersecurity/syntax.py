6930df36ae03fc9e2db877fca70955c4bf76718c
andretdr <162653875+andretdr@users.noreply.github.com>
2024-04-24 07:32:43 +0000

    Wed, Apr 24, 2024, 3:32 PM +08:00

--- cybersecurity/syntax.py
+++ cybersecurity/syntax.py
@@ -6,8 +6,8 @@
 
 # crack.py
 
-from string import #ascii_letters #digits
+from string import ascii_letters, digits, punctuation
 from itertools import product
 
-for passcode in product(ascii_letters, repeat=4):
+for passcode in product(ascii_letters + digits + punctuation, repeat=8):
     print(passcode)
