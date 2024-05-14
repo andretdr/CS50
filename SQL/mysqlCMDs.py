c7e17012d3a0ebc3c2a10d58da0fc2f2cc9bd4b8
andretdr <162653875+andretdr@users.noreply.github.com>
2024-05-14 22:19:25 +0800

    Update mysqlCMDs.py
    adding more syntax

--- SQL/mysqlCMDs.py
+++ SQL/mysqlCMDs.py
@@ -1,3 +1,9 @@
 SHOW DATABASES;
 USE <database name>;
 DESCRIBE <table name>;
+
+UPDATE (fin_holdings INNER JOIN fin_symbs ON fin_holdings.symb_id = fin_symbs.id) 
+SET fin_holdings.quantity = %s, fin_holdings.avgcost = %s 
+WHERE fin_holdings.user_id = %s AND fin_symbs.symb = %s"
+
+
