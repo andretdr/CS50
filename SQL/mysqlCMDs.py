dda87bbdaecb64373903b93df9fc712231e9b8e6
andretdr <162653875+andretdr@users.noreply.github.com>
2024-05-15 10:13:01 +0800

    Update mysqlCMDs.py

--- SQL/mysqlCMDs.py
+++ SQL/mysqlCMDs.py
@@ -6,4 +6,7 @@ UPDATE (fin_holdings INNER JOIN fin_symbs ON fin_holdings.symb_id = fin_symbs.id
 SET fin_holdings.quantity = %s, fin_holdings.avgcost = %s 
 WHERE fin_holdings.user_id = %s AND fin_symbs.symb = %s"
 
-
+"SELECT fin_holdings.avgcost, fin_holdings.quantity, fin_symbs.symb, fin_users.cash 
+FROM ((fin_holdings INNER JOIN fin_users ON fin_holdings.user_id = fin_users.id) 
+      INNER JOIN fin_symbs ON fin_holdings.symb_id = fin_symbs.id) 
+WHERE fin_users.username = %s AND fin_symbs.symb = %s"
